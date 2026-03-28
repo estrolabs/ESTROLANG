8b9683420519cb4b9293ab492dea3a997a46b01c
estrolabs <estrolabs@gmail.com>
2026-03-28 23:41:11 +0000

    Initial Commit!

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ESTROLANG
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("ESTROLANG WELCOME!");
+        }
+    }
+}
